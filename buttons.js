6e44e54bef165e63635779d13f0131c7f6878e63
Robbie <91953410+Robbie-RGU@users.noreply.github.com>
2022-04-03 00:56:23 +0100

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -8,7 +8,25 @@
 		buttons[i].addEventListener("click", changeImages);
 	}
 	
-	
+	///////////////////////////////////////////////////////////////////
+	//Reset any highlighted buttons back to their original color
+	for(let i = 0; i < buttons.length; i++) {
+		buttons[i].style.backgroundColor = "#5FAFE1";
+	}
+					
+	//Get the element of the button that was clicked and highlight it
+	//document.getElementById(e.target.id).style.backgroundColor = "#2C66C4";
+					
+	//Get all the image elements from the image gallary
+	const image = document.getElementsByClassName("ImageGallary");
+						
+	//Swap the images in the image gallary according to what button was clicked
+	for(let i = 0; i < image.length; i++) {
+		image[i].src = `images\\image${i + ((1 * 12) - 12) + 1}.png`;
+		image[i].alt = `Image${i + ((1 * 12) - 12) + 1}`;	
+	}
+	///////////////////////////////////////////////////////////////////////
+
 	function changeImages(e) {
 		
 		//Reset any highlighted buttons back to their original color
