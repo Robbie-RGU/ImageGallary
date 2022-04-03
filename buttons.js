51177a226a4de02ec610f49ee665cb95f948781c
Robbie <91953410+Robbie-RGU@users.noreply.github.com>
2022-04-03 01:07:59 +0100

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -1,4 +1,4 @@
-//window.onload = function() {
+document.onload = function() {
 
 	//Get the button elements
 	const buttons = document.getElementsByClassName("btn");
@@ -28,4 +28,4 @@
 			image[i].alt = `Image${i + ((e.target.id * 12) - 12) + 1}`;	
 		}
 	}
-//}
+}
